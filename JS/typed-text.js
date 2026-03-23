fe3b8ecc93ae30456f886d7ebb482fc7d49435d3
YacoAngeleri <tomasangelerii@gmail.com>
2026-03-23 12:18:19 -0300

    Informacion actualizada y traducida

--- JS/typed-text.js
+++ JS/typed-text.js
@@ -1,6 +1,6 @@
 //Texto Typed
 const typed = new Typed('.typed', {
     strings: ["Estudiante de tercer año de Ingeniería en Sistemas de Información en la UTN, con formación en programación, estructuras de datos, sistemas operativos y arquitectura de computadoras. Busco mi primera experiencia profesional en el área de sistemas para aplicar mis conocimientos, seguir desarrollando mis habilidades técnicas y contribuir al desarrollo de soluciones de software."],
-    typeSpeed: 10,
+    typeSpeed: 12,
     startDelay: 2000,
   });
